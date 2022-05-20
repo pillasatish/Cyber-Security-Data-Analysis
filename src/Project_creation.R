c3097d930c5496ebff28e8813e9ec03d8ae5094d
SATISH <pillasatish7@gmail.com>
2022-05-20 14:40:00 +0100

    created a EDA project(c1047209_PILLA_SATISH_EDA_PROJECT)

---
+++ src/Project_creation.R
@@ -0,0 +1,7 @@
+#Installing the Project Template packages to create a project C1047209%appdata%\RStudio_PILLA_SATISH_EDA_PROJECT
+r = getOption("repos")
+r["CRAN"] = "http://cran.us.r-project.org"
+options(repos = r)
+install.packages("ProjectTemplate")
+library("ProjectTemplate")
+create.project("C1047209_PILLA_SATISH_EDA_PROJECT")
