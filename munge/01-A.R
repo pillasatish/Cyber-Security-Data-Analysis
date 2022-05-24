e5b0e3711c6db302e7b176cb33078cd083a3acd0
SATISH <pillasatish7@gmail.com>
2022-05-24 06:52:29 +0100

    done some intial analysis on enrollment dataset

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,60 @@
-# Example preprocessing script.
+
+#Pre-processing steps:
+
+########################################################################################################
+#                                   F I L E   D E S C R I P T I O N                                    #
+########################################################################################################
+#                                                                                                      #
+# This file contains the code related to Data Munging: transformations like, joining different tables, #
+# merging tables row wise and column wise, text processing, melting table from wide format to long,    #
+# column and row renaming, column and row deletion, calculating different statistics like column mean, #
+# normalizing the data, etc.                                                                           #
+########################################################################################################
+#Reading all the enrollments files 
+
+cyber_security_1_enrolments <- read_csv("data/cyber-security-1_enrolments.csv")
+
+cyber_security_2_enrolments <- read_csv("data/cyber-security-2_enrolments.csv")
+
+cyber_security_3_enrolments <- read_csv("data/cyber-security-3_enrolments.csv")
+
+cyber_security_4_enrolments <- read_csv("data/cyber-security-4_enrolments.csv")
+
+cyber_security_5_enrolments <- read_csv("data/cyber-security-5_enrolments.csv")
+
+cyber_security_6_enrolments <- read_csv("data/cyber-security-6_enrolments.csv")
+
+cyber_security_7_enrolments <- read_csv("data/cyber-security-7_enrolments.csv")
+
+enrolments = rbind(cyber_security_1_enrolments, cyber_security_2_enrolments, cyber_security_3_enrolments, cyber_security_4_enrolments, cyber_security_5_enrolments, cyber_security_6_enrolments, cyber_security_7_enrolments)
+#enrolments
+
+
+# Finding the total number of enrollments over the course run  
+enrollment_data=list(cyber_security_1_enrolments,cyber_security_2_enrolments,cyber_security_3_enrolments,cyber_security_4_enrolments,cyber_security_5_enrolments,cyber_security_6_enrolments,cyber_security_7_enrolments)
+# to get a list of enrollments from each of the file 
+student_count=lapply(enrollment_data, function(x) length(unique(x$learner_id)))
+student_count =  array(as.numeric(unlist(student_count)), dim = c(7,1,1))
+#student_enrollnmnent_cnt_un
+stud_count = data.frame(student_count)
+#adding an additional column run to plot the graph 
+stud_count = tibble::rownames_to_column(stud_count, "Runs")
+stud_count
+
+
+#Plotting the percentage of students joining in each Iteration of the course.
+data <- stud_count$student_count
+data
+chapters<-c('1st batch','2nd batch','3rd batch','4th batch','5th batch','6th batch','7th batch')
+#calculates the percentage of the values
+percentage<-round(data/sum(data)*100)
+
+#concatenates the strings with percentages
+labels_new<-paste(chapters,percentage)
+labels_new
+
+#concatenates the above output with the '%' symbol 
+final_labels<-paste(labels_new,'%',sep = "")
+final_labels
+lab <- paste0(round(data/sum(data) * 100, 2), "%")
+lab
--- src/eda.R
+++ src/eda.R
@@ -1,17 +1,15 @@
 # plotting the enrollments 
-enrollments_plot=ggplot(stud_count,aes(Runs,student_count,student_count))+geom_bar(stat="identity", col = viridis(7), alpha=.8, width=.5)+
+enrollments_plot=ggplot(data=stud_count,aes(Runs,student_count,student_count))+geom_bar(stat="identity", fill = viridis(7), alpha=.8, width=.5)+
   geom_text(aes(label = student_count, vjust = 1))+
   ggtitle("Number of Learners Enrolled Over Different Iteration")+
   xlab("Batches") +
   ylab("Number of Enrolment(learners)")+
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5,face = 'bold'))
 enrollments_pt=enrollments_plot+ geom_line(data = stud_count, mapping = aes(x = Runs, y = student_count, group =1), col = "blue",size = 1.5)
 enrollments_pt
 
 
 #Percentage of Learners Enrolled Over Each Batch
-enrolled_3D_plot=pie3D(data,labels = final_labels,explode = 0.05,main='Percentage of Learners Enrolled Over Each Batch',labelcex =1.2,shade =1.75,radius =1)
-
-
+pie3d=pie3D(data,labels = final_labels,explode = 0.05,main='Percentage of Learners Enrolled Over Each Batch',labelcex =1.2,shade =1.75,radius =1)
 
 
